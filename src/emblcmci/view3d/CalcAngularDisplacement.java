5b0671d28f9adfccf817d4096e104d29dccac204
Kota Miura <miura@embl.de>
2012-03-27 17:47:44 +0200

    added calc angular displacmeent class, for a new calculation of displacments.

---
+++ src/emblcmci/view3d/CalcAngularDisplacement.java
@@ -0,0 +1,23 @@
+package emblcmci.view3d;
+
+import org.apache.commons.math.geometry.euclidean.threed.Vector3D;
+
+import emblcmci.view3d.Plot4d.DispVec;
+
+public class CalcAngularDisplacement {
+	
+	/** Calculates angular displacement vector towards a reference bar.
+	 * returned value is an DispVec class holding containing displacement vector and direction (1 or -1) 
+	 * 
+	 * @param sev track vector, between start and end
+	 * @param rv reference vector (single point, center of the sphere)
+	 * @return DispVec instance, holding a Vector3D, the net displacement vector towards a point and its direction
+	 * <br>
+	 * <br>	
+	 */
+	public DispVec calcAngularVector(Vector3D sev, Vector3D rv){
+		
+		return null;
+	}
+
+}
